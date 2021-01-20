cd757a72306cb19087e425d8e8fc91cd6a864c35
ahmed-razic <ahmed.razic@gmail.com>
2021-01-20 21:57:59 +0100

    modified layout, again

--- app/src/main/java/com/example/android/spacequiz/MainActivity.java
+++ app/src/main/java/com/example/android/spacequiz/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.android.spacequiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int questionNumber = 1;
+    int currentScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
