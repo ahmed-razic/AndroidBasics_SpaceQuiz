62a53a9857d7fbc7fcc3bbea7b6c120896daee8e
ahmed-razic <ahmed.razic@gmail.com>
2021-01-21 00:57:25 +0100

    added reset button

--- app/src/main/java/com/example/android/spacequiz/MainActivity.java
+++ app/src/main/java/com/example/android/spacequiz/MainActivity.java
@@ -11,6 +11,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int score = 0;
+    EditText inputOne;
+    EditText inputTwo;
+    RadioButton answer3;
+    RadioButton answer3Wrong;
+    CheckBox answer4;
+    CheckBox answer4Wrong;
+    EditText inputFive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +25,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void submit(View view) {
+    public void submit(View v) {
 
-        EditText inputOne = findViewById(R.id.input1);
+        inputOne = findViewById(R.id.input1);
         String answer1 = inputOne.getText().toString();
-        EditText editView2 = findViewById(R.id.input2);
-        String answer2 = editView2.getText().toString();
-        RadioButton answer3 = findViewById(R.id.radioButton4);
-        CheckBox answer4 = findViewById(R.id.checkBox1);
-        EditText editView5 = findViewById(R.id.input5);
-        String answer5 = editView5.getText().toString();
+        inputTwo = findViewById(R.id.input2);
+        String answer2 = inputTwo.getText().toString();
+        answer3 = findViewById(R.id.radioButton4);
+        answer3Wrong = findViewById(R.id.radioButton5);
+        answer4 = findViewById(R.id.checkBox1);
+        answer4Wrong = findViewById(R.id.checkBox2);
+        inputFive = findViewById(R.id.input5);
+        String answer5 = inputFive.getText().toString();
 
         if(answer1.equals("andromeda")) {
             score = score + 1;
@@ -45,6 +54,20 @@ public class MainActivity extends AppCompatActivity {
             score = score + 1;
         }
 
+        display();
+        score = 0;
+    }
+
+    public void reset(View v) {
+
+        inputOne.setText("");
+        inputTwo.setText("");
+        answer3.setChecked(false);
+        answer3Wrong.setChecked(false);
+        answer4.setChecked(false);
+        answer4Wrong.setChecked(false);
+        inputFive.setText("");
+        score = 0;
         display();
     }
 
